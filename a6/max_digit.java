9beea6e82306b4c3bb4c89c5c802e6efbbd078ae
sarishinohara <sgmsuchan@gmail.com>
2017-04-21 19:02:06 +0200

    fix a6

--- a6/max_digit.java
+++ a6/max_digit.java
@@ -1,49 +1,24 @@
 import java.util.*;
 
-class Solution
-{
-  /**
-  * Returns the maximum possible integer given the string str containing digits, using 
-  * maximum of n digits. Each digit in str can only be used once. 
-  **/
-	static int maxDigit(String str, int n) {
-	      if(str == null ||n <= 0) {
-	        return -1;
-	      }
-	      if (!str.matches("\\d+")) {
-            return -1;
-        }
-	      
-	      if(n > str.length()) {
-            n = str.length();
-        }
-        Set<Integer> result = new HashSet<>();
-        maxDigitHelper(str, n, "", result);
-        int max = Collections.max(result);
-        return max;
-	}
-	
-	/**
-  * Creates a set of permutations in result based on string str with max n digits.
-  **/
-  static void maxDigitHelper(String str, int n, String acc, Set<Integer> result) {
-        if (n == 0) {
-            permutation("", acc, result);
-        }
-        else {
-            for (int i = 0; i < str.length(); i++)
-                maxDigitHelper(str.substring(i + 1),  n-1, acc + str.charAt(i), result);
-        }
+class Solution {
+  
+  static Set<String> permuteString(String s) {
+    
+    Set<String> result = new TreeSet<String>();
+    // call backtracking method here
+    findPermutations(s,result,"");
+    return result;
   }
   
-    static void permutation(String prefix, String str, Set<Integer>result) {
-        int n = str.length();
-        if (n == 0) {System.out.println(prefix); result.add(Integer.parseInt(prefix));}
-        else {
-            for (int i = 0; i < n; i++)
-                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n),result);
-        }
+  static void findPermutations(String s, Set<String> result, String string){
+      if(s.length() < 1) {
+          result.add(string);
+      } else {
+           for (int i = 0; i < s.length(); i++) {
+              String sub = s.substring(i, i + 1);
+              String rest = s.substring(0, i) + s.substring(i + 1);
+              findPermutations(rest, result,string + sub);
+          }
+      }
     }
-  
 }
-//
--- a6/permutation_string.java
+++ a6/permutation_string.java
@@ -1,34 +1,47 @@
+package weblab;
 import java.util.*;
 
-class Solution {
-  
-  static Set<String> permuteString(String s) {
-    
-    Set<String> result = new TreeSet<String>();
-    
-    if (s == null) {
-            return null;
-        } else if (s.length() == 0) {
-            result.add("");
-            return result;
+class Solution
+{
+  /**
+  * Returns the maximum possible integer given the string str containing digits, using 
+  * maximum of n digits. Each digit in str can only be used once. 
+  **/
+	static int maxDigit(String str, int n) {
+Set<Integer> result = new TreeSet<Integer>();
+	      if(str == null ||n <= 0) {
+	        return -1;
+	      }
+	      if (!str.matches("\\d+")) {
+            return -1;
         }
-        char initial = s.charAt(0); // first character
-        String rem = s.substring(1); // Full string without first character
-        Set<String> words = permuteString(rem);
-        for (String strNew : words) {
-            for (int i = 0;i<=strNew.length();i++){
-                result.add(findPermutations(strNew, initial, i));
+      
+        maxDigitHelper(str, n, "", result);
+        Iterator<Integer> it = result.iterator();
+        int max = it.next();
+
+        while (it.hasNext()) {
+            int comp = it.next();
+            if (comp > max) {
+                max = comp;
             }
         }
-        return result;
+        return max;
+	}
+	
+	/**
+  * Creates a set of permutations in result based on string str with max n digits.
+  **/
+  static void maxDigitHelper(String str, int n, String acc, Set<Integer> result) {
+        if (acc.length() == n || str.length() == 0) {
+              result.add(Integer.parseInt(acc));
+        } else {
+            for (int i = 0; i < str.length(); i++) {
+                String digit = str.substring(i, i + 1);
+                String rest = str.substring(0, i) + str.substring(i + 1, str.length());
+                String newString = acc + digit;
+                maxDigitHelper(rest, n, newString, result);
+            }
+        }    
   }
-  
-      public static String findPermutations(String str, char c, int j) {
-        String begin = str.substring(0, j);
-        String end = str.substring(j);
-        return begin + c + end;
-    }
-  
-  
-  
-}//
+}
